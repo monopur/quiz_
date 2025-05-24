ce962fc51f18cbfdbd46029f8af0b2dea47f283f
monopur <topuzm@yandex.com>
2025-05-24 13:09:55 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # QuizPi Flask Sunucusunu Başlatır
 
-cd ../backend
+cd "$(dirname "$0")/../backend"
 export FLASK_APP=app.py
 export FLASK_ENV=production
 flask run --host=0.0.0.0 --port=5000
